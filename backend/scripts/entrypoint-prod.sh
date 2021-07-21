2afcbc3d0836f2ddfaad7c9c758d4b24e53e19d9
Alschn <adam.lisichin@gmail.com>
2021-07-21 22:45:48 +0200

    Update entrypoint-prod.sh

--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -2,4 +2,4 @@
 python3 backend/manage.py makemigrations --no-input
 python3 backend/manage.py migrate --no-input
 
-gunicorn --chdir backend core.wsgi
+daphne -b 0.0.0.0 -p 8001 backend.core.asgi:application
