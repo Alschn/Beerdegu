a5f091a3e11800744012498243303f5f944ebca6
Alschn <adam.lisichin@gmail.com>
2021-07-21 23:24:08 +0200

    Update deploy settings

--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -2,4 +2,4 @@
 python3 backend/manage.py makemigrations --no-input
 python3 backend/manage.py migrate --no-input
 
-daphne -b 0.0.0.0 -p 8001 backend.core.asgi:application
+daphne -b 0.0.0.0 backend/core.asgi:application -v2
