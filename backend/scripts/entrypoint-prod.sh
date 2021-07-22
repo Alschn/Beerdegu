da14097339fb7047b50b708a1a6218111c0a311b
Alschn <adam.lisichin@gmail.com>
2021-07-22 11:47:01 +0200

    Update entrypoint-prod.sh
    Add superuser creation script, cd backend at the top.

--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-python3 backend/manage.py makemigrations --no-input
-python3 backend/manage.py migrate --no-input
-
 cd backend
+python3 manage.py makemigrations --no-input
+python3 manage.py migrate --no-input
+
+script="
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD');
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python manage.py shell
+
 daphne -b 0.0.0.0 -p $PORT core.asgi:application -v2
