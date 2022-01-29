21f9a7b3ec3ee6ac7d131e3225b24085225d3b59
Alschn <adam.lisichin@gmail.com>
2022-01-29 16:14:07 +0100

    Add custom model admins
    Added model admin for each model with custom list displays and inherited from admins from django-import-export.

--- backend/beers/admin.py
+++ backend/beers/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
+from import_export.admin import ImportExportActionModelAdmin
+
 from .models import Beer, BeerStyle, Brewery, Hop
 
-admin.site.register([Beer, BeerStyle, Brewery, Hop])
+
+class BeerAdmin(ImportExportActionModelAdmin):
+    list_display = ('id', 'name', 'brewery', 'style', 'percentage', 'volume_ml')
+
+
+class BreweryAdmin(ImportExportActionModelAdmin):
+    list_display = ('id', 'name', 'city', 'country', 'established')
+
+
+class BeerStyleAdmin(ImportExportActionModelAdmin):
+    list_display = ('id', 'name')
+
+
+class HopAdmin(ImportExportActionModelAdmin):
+    list_display = ('id', 'name', 'country')
+
+
+admin.site.register(Beer, BeerAdmin)
+admin.site.register(Brewery, BreweryAdmin)
+admin.site.register(BeerStyle, BeerStyleAdmin)
+admin.site.register(Hop, HopAdmin)
--- backend/rooms/admin.py
+++ backend/rooms/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
+from django.db.models import Avg
+from import_export.admin import ImportExportActionModelAdmin
 
 from .models import Room, Rating, UserInRoom, BeerInRoom
 
 
-class UserInRoomAdmin(admin.ModelAdmin):
+@admin.register(Room)
+class RoomAdmin(ImportExportActionModelAdmin):
+    list_display = ('id', 'name', 'host', 'slots', 'state', 'password')
+
+
+@admin.register(Rating)
+class RatingAdmin(ImportExportActionModelAdmin):
+    pass
+
+
+@admin.register(BeerInRoom)
+class BeerInRoomAdmin(ImportExportActionModelAdmin):
+    list_display = ('id', 'room', 'beer', 'average_note')
+
+    def average_note(self, obj: BeerInRoom):
+        if obj.ratings:
+            return obj.ratings.aggregate(Avg('note'))['note__avg']
+        return None
+
+
+@admin.register(UserInRoom)
+class UserInRoomAdmin(ImportExportActionModelAdmin):
+    list_display = ('id', 'room', 'user', 'joined_at', 'last_active')
     readonly_fields = ('joined_at', 'last_active',)
-
-
-admin.site.register([Room, Rating, BeerInRoom])
-admin.site.register(UserInRoom, UserInRoomAdmin)
