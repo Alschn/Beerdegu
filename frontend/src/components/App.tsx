1aac07e8bb9f83336616ec1f39047b229c63b048
Alschn <adam.lisichin@gmail.com>
2022-07-04 22:08:47 +0200

    Install react-query

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
 import Router from "../routing/Router";
 import {ThemeProvider, createTheme} from '@mui/material/styles';
+import {QueryClient, QueryClientProvider} from "react-query";
 
 const theme = createTheme();
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router/>
+      <QueryClientProvider client={queryClient}>
+        <Router/>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
