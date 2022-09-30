4644aabaf6f12410646c3dd25b1a9b67ebf184b4
Alschn <67019937+Alschn@users.noreply.github.com>
2022-09-30 15:34:04 +0200

    Improve readability in permissions

--- backend/rooms/permissions.py
+++ backend/rooms/permissions.py
@@ -10,20 +10,34 @@ ALLOWED_METHODS = ['GET', 'HEAD', 'OPTIONS', 'POST']
 
 class IsHostOrListCreateOnly(BasePermission):
     def has_object_permission(self, request: Request, view: APIView, obj: Room) -> bool:
-        if request.method in PROTECTED_METHODS:
-            if obj.host and obj.host == request.user:
-                return True
-        elif request.method in ALLOWED_METHODS:
+        # safe methods + POST to create a new room
+        if request.method in ALLOWED_METHODS:
             return True
+
+        # host condition
+        elif request.method in PROTECTED_METHODS and (obj.host and obj.host == request.user):
+            return True
+
+        # otherwise permission denied
         return False
 
 
 class IsHostOrListOnly(BasePermission):
     def has_permission(self, request: Request, view: APIView) -> bool:
+        # list condition
         if request.method == 'GET':
             return True
-        room = Room.objects.filter(name=view.kwargs.get('room_name'))
-        if room.exists():
-            if room.first().host != request.user:
-                return False
-        return True
+
+        room = Room.objects.filter(name=view.kwargs['room_name'])
+        room_exists = room.exists()
+
+        # host condition
+        if room_exists and room.first().host == request.user:
+            return True
+
+        # let view handle 404 instead of permission raising 403
+        elif not room_exists:
+            return True
+
+        # otherwise, permission denied
+        return False
