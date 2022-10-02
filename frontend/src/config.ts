709a54f17ff577099c759da405980bfca2706648
Alschn <67019937+Alschn@users.noreply.github.com>
2022-10-02 17:04:20 +0200

    Remove build time args which are not working

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,11 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL as string;
-const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKET_URL as string;
+let BACKEND_URL = process.env.REACT_APP_BACKEND_URL as string;
+let WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKET_URL as string;
 
+if (process.env.NODE_ENV === 'production') {
+  // had to use this hack because of heroku cannot use config vars build time for some stupid reason
+  BACKEND_URL = 'https://beerdegu.herokuapp.com';
+  WEBSOCKET_URL = 'wss://beerdegu.herokuapp.com';
+}
 
 if (!BACKEND_URL) {
   throw new Error('Missing `REACT_APP_BACKEND_URL` environment variable. Example `http://127.0.0.1:8000`');
