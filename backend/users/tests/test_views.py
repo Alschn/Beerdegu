b61fbc6c6bc1848bb479257af5d156c4207742a2
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-05 22:45:52 +0200

    Update auth tests

--- backend/users/tests/test_views.py
+++ backend/users/tests/test_views.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.models import User
-from django.urls import reverse
 from django.test import TestCase
 from rest_framework import status
+from rest_framework.reverse import reverse_lazy
 from rest_framework.test import APIClient
 
 from users.serializers.user import UserSerializer
 
 
 class AuthViewsTests(TestCase):
+    register_url = reverse_lazy('auth-register')
+    jwt_login_url = reverse_lazy('auth-jwt-login')
+    logout_url = reverse_lazy('auth-logout')
+
     def setUp(self) -> None:
         self.client = APIClient()
 
@@ -19,7 +23,7 @@ class AuthViewsTests(TestCase):
         )
 
     def test_register_success(self):
-        response = self.client.post(reverse('auth-register'), {
+        response = self.client.post(self.register_url, {
             'username': 'Test2',
             'email': 'test2@gmail.com',
             'password1': 'verysecretpassword123',
@@ -30,7 +34,7 @@ class AuthViewsTests(TestCase):
         self.assertEqual(response.json(), UserSerializer(user).data)
 
     def test_register_invalid_email(self):
-        response = self.client.post(reverse('auth-register'), {
+        response = self.client.post(self.register_url, {
             'username': 'Halo',
             'email': 'abc',
             'password1': '123ogpasd2',
@@ -40,7 +44,7 @@ class AuthViewsTests(TestCase):
         self.assertEqual(response.json(), {'email': ['Enter a valid email address.']})
 
     def test_register_passwords_not_matching(self):
-        response2 = self.client.post(reverse('auth-register'), {
+        response2 = self.client.post(self.register_url, {
             'username': 'Halo',
             'email': 'abc@gmail.com',
             'password1': '123ogpasd2',
@@ -50,7 +54,7 @@ class AuthViewsTests(TestCase):
         self.assertEqual(response2.json(), {'non_field_errors': ["The two password fields didn't match."]})
 
     def test_register_user_already_exists(self):
-        response = self.client.post(reverse('auth-register'), {
+        response = self.client.post(self.register_url, {
             'username': 'Test',
             'email': 'test@gmail.com',
             'password1': '!@#sdafggdf',
@@ -60,7 +64,7 @@ class AuthViewsTests(TestCase):
         self.assertEqual(response.json(), {'username': ['A user with that username already exists.']})
 
     def test_login_success(self):
-        response = self.client.post(reverse('auth-jwt-login'), {
+        response = self.client.post(self.jwt_login_url, {
             'username': 'Test',
             'password': 'abcdefg',
         })
@@ -70,21 +74,21 @@ class AuthViewsTests(TestCase):
         self.assertTrue('refresh' in response_json)
 
     def test_login_invalid_data(self):
-        response = self.client.post(reverse('auth-jwt-login'), {
+        response = self.client.post(self.jwt_login_url, {
             'username': 'Test',
             'password': 'xd',
         })
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertEqual(response.json(), {'detail': 'No active account found with the given credentials'})
+        self.assertIn('detail', response.json())
 
     def test_logout(self):
         self.client.login(username='Test', password='abcdefg')
         self.client.force_authenticate(self.user)
-        response = self.client.post(reverse('auth-logout'), {})
+        response = self.client.post(self.logout_url, {})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json(), {'message': 'Successfully logged out.'})
 
     def test_logout_unauthorized(self):
-        response = self.client.post(reverse('auth-logout'), {})
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertEqual(response.json(), {'detail': 'Authentication credentials were not provided.'})
+        response = self.client.post(self.logout_url, {})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertIn('detail', response.json())
