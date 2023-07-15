921e196e0c8ae258dfcad9ac823dd56a46a19c44
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-15 22:48:23 +0200

    Configure required email verification

--- backend/users/forms/password_reset.py
+++ backend/users/forms/password_reset.py
@@ -2,19 +2,19 @@ from allauth.account import app_settings
 from allauth.account.adapter import get_adapter
 from allauth.account.forms import default_token_generator
 from allauth.account.utils import user_pk_to_url_str, user_username
-from allauth.utils import build_absolute_uri
 from dj_rest_auth.forms import AllAuthPasswordResetForm
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 
 
 class UserPasswordResetForm(AllAuthPasswordResetForm):
-    frontend_path = '/password/reset/confirm/{uid}/{token}/'
 
     def save(self, request, **kwargs):
-        current_site = get_current_site(request)
         email = self.cleaned_data['email']
         token_generator = kwargs.get('token_generator', default_token_generator)
+
+        adapter = get_adapter(request)
+        assert hasattr(adapter, 'get_password_reset_url'), 'get_password_reset_url() must be implemented in Adapter'
 
         for user in self.users:
             temp_key = token_generator.make_token(user)
@@ -23,19 +23,14 @@ class UserPasswordResetForm(AllAuthPasswordResetForm):
             # password_reset = PasswordReset(user=user, temp_key=temp_key)
             # password_reset.save()
 
-            # send the password reset email
-            path = self.frontend_path.format(
+            url = adapter.get_password_reset_url(
+                request,
                 uid=user_pk_to_url_str(user),
                 token=temp_key
             )
 
-            if getattr(settings, 'REST_AUTH_PW_RESET_USE_SITES_DOMAIN', False) is True:
-                url = build_absolute_uri(None, path)
-            else:
-                url = build_absolute_uri(request, path)
-
             context = {
-                'current_site': current_site,
+                'current_site': settings.FRONTEND_SITE_NAME,
                 'user': user,
                 'password_reset_url': url,
                 'request': request,
@@ -43,7 +38,7 @@ class UserPasswordResetForm(AllAuthPasswordResetForm):
             if app_settings.AUTHENTICATION_METHOD != app_settings.AuthenticationMethod.EMAIL:
                 context['username'] = user_username(user)
 
-            get_adapter(request).send_mail(
+            adapter.send_mail(
                 'account/email/password_reset_key', email, context
             )
 
