fdad8aa2fe9dd472d12ff25c793319bf6e59ea22
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-19 00:18:23 +0200

    Add missing ToastContainer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AuthProvider from "./context/authContext";
+import AuthProvider from './context/authContext';
 import {ThemeProvider} from '@mui/material/styles';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import theme from "./theme";
-import Router from "./routing/Router";
-import "./main.scss";
+import Router from './routing/Router';
+import './main.scss';
 import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        pauseOnHover={false}
+        pauseOnFocusLoss={false}
+      />
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <Router/>
