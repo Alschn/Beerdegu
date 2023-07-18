5d8271f17d3dfc292e33ec5bcf5e70deaaaadbc4
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-18 22:45:19 +0200

    Try to disable session auth on verify email endpoint

--- backend/users/views/verify_email.py
+++ backend/users/views/verify_email.py
@@ -5,4 +5,5 @@ class VerifyEmailAPIView(VerifyEmailView):
     """
     POST /api/auth/register/confirm-email/
     """
-    pass
+    # disable authentication for this endpoint
+    authentication_classes = []
