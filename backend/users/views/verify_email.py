574d09e548bfa69e573b63775e484aa8be2a6ff7
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-18 22:37:01 +0200

    Add rest framework prod settings

--- backend/users/views/verify_email.py
+++ backend/users/views/verify_email.py
@@ -1,16 +1,8 @@
-from typing import Any
-
 from dj_rest_auth.registration.views import VerifyEmailView
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.request import Request
-from rest_framework.response import Response
 
 
 class VerifyEmailAPIView(VerifyEmailView):
     """
     POST /api/auth/register/confirm-email/
     """
-
-    @csrf_exempt
-    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        return super().post(request, *args, **kwargs)
+    pass
