76345bd223431acaf016da01b6d754e7782b9358
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-18 22:19:36 +0200

    Add csrf_exempt to prevent api error

--- backend/users/views/verify_email.py
+++ backend/users/views/verify_email.py
@@ -1,8 +1,16 @@
+from typing import Any
+
 from dj_rest_auth.registration.views import VerifyEmailView
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 
 class VerifyEmailAPIView(VerifyEmailView):
     """
     POST /api/auth/register/confirm-email/
     """
-    pass
+
+    @csrf_exempt
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        return super().post(request, *args, **kwargs)
