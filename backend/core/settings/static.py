31114aec90a2b4515c46c830561ecb470dba1c3d
Alschn <67019937+Alschn@users.noreply.github.com>
2023-08-02 18:37:57 +0200

    Add static/media files integration with AWS S3
    Make it possible to collectstatic and upload media files into AWS S3 bucket.

--- backend/core/settings/static.py
+++ backend/core/settings/static.py
@@ -8,22 +8,57 @@ from core.settings.base import *
 
 ALLOWED_HOSTS = []
 
-ROOT_DIR = BASE_DIR.parent.parent
+ROOT_DIR = BASE_DIR.parent
 
 SECRET_KEY = get_random_secret_key()
 
 DEBUG = False
 
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+USE_AWS_S3 = os.getenv('USE_S3', 'FALSE').lower() == 'true'
 
-TEMPLATES[0]["DIRS"] = [os.path.join(ROOT_DIR, 'frontend', 'build')]
+if USE_AWS_S3:
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': f'max-age={24 * 3600}'}
 
-STATICFILES_DIRS = [os.path.join(ROOT_DIR, 'frontend', 'build', 'assets')]
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    PUBLIC_MEDIA_LOCATION = 'uploads'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    MEDIA_ROOT = None
 
-STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
+    STORAGES = {
+        'default': {
+            'BACKEND': 'core.shared.storages.PublicMediaStorage'
+        },
+        'staticfiles': {
+            'BACKEND': 'core.shared.storages.StaticStorage'
+        },
+    }
+else:
+    MIDDLEWARE.insert(3, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
-STATIC_URL = '/static/'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-WHITENOISE_ROOT = os.path.join(ROOT_DIR, 'frontend', 'build')
+    STORAGES = {
+        'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        }
+    }
+
+    MEDIA_URL = '/uploads/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+if SERVE_FRONTEND:
+    WHITENOISE_ROOT = os.path.join(ROOT_DIR, 'frontend', 'build')
+    STATICFILES_DIRS = [
+        os.path.join(ROOT_DIR, 'frontend', 'build', 'assets')
+    ]
+    TEMPLATES[0]['DIRS'] = [
+        os.path.join(ROOT_DIR, 'frontend', 'build')
+    ]
