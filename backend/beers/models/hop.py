dc5187b2c57ab67d191c1687d9117b76edbbf4b2
Alschn <67019937+Alschn@users.noreply.github.com>
2023-08-15 12:54:37 +0200

    Update country field on Hop model
    Used CountryField from django-countries

--- backend/beers/models/hop.py
+++ backend/beers/models/hop.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
 
 
 class Hop(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=1000, null=True, blank=True)
-    country = models.CharField(max_length=100, null=True, blank=True)
+    country = CountryField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = _('Hop')
+        verbose_name_plural = _('Hops')
+
     def __str__(self) -> str:
-        return f"{self.name}"
+        return self.name
