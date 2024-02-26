1f9a8af7afbb6f418c866a3a57c73d18109ad2d3
Alschn <67019937+Alschn@users.noreply.github.com>
2024-02-26 14:08:54 +0100

    Add purchased_at validation in create serializer

--- backend/purchases/serializers/beer_purchase.py
+++ backend/purchases/serializers/beer_purchase.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from purchases.models import BeerPurchase
@@ -31,3 +35,15 @@ class BeerPurchaseCreateSerializer(serializers.ModelSerializer):
             'image',
             'purchased_at',
         )
+        read_only_fields = ('id', 'sold_to')
+
+    # noinspection PyMethodMayBeStatic
+    def validate_purchased_at(self, value: datetime.date) -> datetime.date:
+        now = timezone.now()
+
+        if value > now.date():
+            raise serializers.ValidationError(
+                _('Cannot purchase beer in the future')
+            )
+
+        return value
