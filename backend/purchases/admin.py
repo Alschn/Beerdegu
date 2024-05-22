ff942dcc00ad3630ba2479cb2a06ee8c0d65909f
Alschn <67019937+Alschn@users.noreply.github.com>
2024-05-22 15:32:34 +0200

    Add `sold_to` to list_display in BeerPurchase admin

--- backend/purchases/admin.py
+++ backend/purchases/admin.py
@@ -6,7 +6,7 @@ from purchases.models import BeerPurchase
 @admin.register(BeerPurchase)
 class BeerPurchaseAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'beer', 'packaging',
+        'id', 'sold_to', 'beer', 'packaging',
         'volume_ml', 'price', 'purchased_at'
     )
     list_select_related = ('beer', 'sold_to')
