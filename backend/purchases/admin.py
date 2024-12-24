80b9294210c29b9d05785982de467c9df042d34f
Alschn <67019937+Alschn@users.noreply.github.com>
2024-12-24 15:52:33 +0100

    Add BeerPurchaseInline in BeerAdmin, RatingInline in BeerPurchaseAdmin

--- backend/purchases/admin.py
+++ backend/purchases/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
+from django.forms import BaseModelFormSet, BaseModelForm
 
 from purchases.models import BeerPurchase
+from ratings.models import Rating
+
+
+class RatingInline(admin.StackedInline):
+    model = Rating
+    extra = 0
+    fk_name = 'beer_purchase'
+    fields = (
+        # beer is derived from parent BeerPurchase
+        'added_by',
+        'room',
+        'color',
+        'foam',
+        'smell',
+        'taste',
+        'opinion',
+        'note',
+        'is_published'
+    )
+
+    def has_view_or_change_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(BeerPurchase)
@@ -14,6 +37,18 @@ class BeerPurchaseAdmin(admin.ModelAdmin):
         'beer__name', 'beer__style__name',
         'beer__brewery__name', 'sold_to__username'
     )
+    inlines = [RatingInline]
+
+    def save_formset(self, request, form: BaseModelForm, formset: BaseModelFormSet, change: bool) -> None:
+        obj: BeerPurchase = form.instance
+
+        for form in formset.extra_forms:
+            # derive rated beer from parent BeerPurchase model,
+            # so that we do not need to select it manually
+            if isinstance(form.instance, Rating):
+                form.instance.beer = obj.beer
+
+        super().save_formset(request, form, formset, change)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         form_field = super().formfield_for_foreignkey(db_field, request, **kwargs)
